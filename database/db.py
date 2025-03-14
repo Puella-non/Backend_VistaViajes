d5183eccd8ea719141c22dc2d9c2ac5592d896cf
Puella-non <Laura.agudelo-mo@uniminuto.edu.co>
2025-03-14 15:06:34 -0500

    Archivo de requerimientos

--- database/db.py
+++ database/db.py
@@ -3,6 +3,6 @@ from flask_mysqldb import MySQL
 def init_database(app):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = '12345678'
+    app.config['MYSQL_PASSWORD'] = 'Puella_non1987'
     app.config['MYSQL_DB'] = 'vistaViajes'
     return MySQL(app)
